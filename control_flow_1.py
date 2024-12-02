9796e6453a71b2e974f6e835f8d46ad39ac0ae9b
Samodi1 <111043853+Samodi1@users.noreply.github.com>
2024-12-02 20:24:04 +0000

    Add files via upload

---
+++ control_flow_1.py
@@ -0,0 +1,22 @@
+#Assignments
+#Q1 Write a program that print all the odd numbers between variable A and Variable B
+
+odd_num = "variable A and variable B"
+print(odd_num [1: :2])
+
+#Q2 Write a program giving a variable Ss print all the even characters
+even_num = "variable ss"
+print(even_num [0: :2])
+# ss = 0
+# while ss != "":
+#     ss += 2
+#     if ss <= 1000:
+#         print(ss)
+
+# #or
+# ss = 0
+# while ss >= 0:
+#     ss += 2
+#     if ss <= 1000:
+#         print(ss)
+
